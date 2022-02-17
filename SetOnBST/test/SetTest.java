f15c02686a8c1e6aaffcf98d314c9d12f8347939
tomaszfrelek2 <98548855+tomaszfrelek2@users.noreply.github.com>
2022-02-17 12:00:41 -0500

    Updated test cases
    The expected values where calling "createFromArgsTest()" instead of "createFromArgsRef()"

--- SetOnBST/test/SetTest.java
+++ SetOnBST/test/SetTest.java
@@ -76,7 +76,7 @@ public abstract class SetTest {
     @Test
     public void testConstructorTestWithContents() {
         Set<String> s1 = this.createFromArgsTest("5", "6");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6");
         assertEquals(s1Expected, s1);
     }
 
@@ -84,7 +84,7 @@ public abstract class SetTest {
     @Test
     public void testConstructorTestEmpty() {
         Set<String> s1 = this.createFromArgsTest();
-        Set<String> s1Expected = this.createFromArgsTest();
+        Set<String> s1Expected = this.createFromArgsRef();
         assertEquals(s1Expected, s1);
     }
 
@@ -92,7 +92,7 @@ public abstract class SetTest {
     @Test
     public void testAddTwoEntries() {
         Set<String> s1 = this.createFromArgsTest("5");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6");
         s1.add("6");
         assertEquals(s1Expected, s1);
     }
@@ -101,7 +101,7 @@ public abstract class SetTest {
     @Test
     public void testAddOneEntry() {
         Set<String> s1 = this.createFromArgsTest("5", "7");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6", "7");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6", "7");
         s1.add("6");
         assertEquals(s1Expected, s1);
     }
@@ -110,7 +110,7 @@ public abstract class SetTest {
     @Test
     public void testAddEmpty() {
         Set<String> s1 = this.createFromArgsTest();
-        Set<String> s1Expected = this.createFromArgsTest("6");
+        Set<String> s1Expected = this.createFromArgsRef("6");
         s1.add("6");
         assertEquals(s1Expected, s1);
     }
@@ -119,7 +119,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveOneEntry() {
         Set<String> s1 = this.createFromArgsTest("6");
-        Set<String> s1Expected = this.createFromArgsTest();
+        Set<String> s1Expected = this.createFromArgsRef();
         String temp = s1.remove("6");
         assertEquals(s1Expected, s1);
         assertEquals("6", temp);
@@ -129,7 +129,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveTwoEntries() {
         Set<String> s1 = this.createFromArgsTest("6", "7");
-        Set<String> s1Expected = this.createFromArgsTest("7");
+        Set<String> s1Expected = this.createFromArgsRef("7");
         String temp = "6";
         String temp2 = s1.remove(temp);
         assertEquals(s1Expected, s1);
@@ -140,7 +140,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveThreeEntries() {
         Set<String> s1 = this.createFromArgsTest("6", "7", "8");
-        Set<String> s1Expected = this.createFromArgsTest("7", "8");
+        Set<String> s1Expected = this.createFromArgsRef("7", "8");
         String temp = "6";
         String temp2 = s1.remove(temp);
         assertEquals(s1Expected, s1);
@@ -151,7 +151,7 @@ public abstract class SetTest {
     @Test
     public void testContainsNotIn() {
         Set<String> s1 = this.createFromArgsTest("5", "6", "7");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6", "7");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6", "7");
         boolean temp = s1.contains("4");
         assertEquals(s1Expected, s1);
         assertEquals(false, temp);
@@ -162,7 +162,7 @@ public abstract class SetTest {
     @Test
     public void testContainsTwoEntries() {
         Set<String> s1 = this.createFromArgsTest("6", "7");
-        Set<String> s1Expected = this.createFromArgsTest("6", "7");
+        Set<String> s1Expected = this.createFromArgsRef("6", "7");
         boolean temp = s1.contains("6");
         assertEquals(s1Expected, s1);
         assertEquals(true, temp);
@@ -172,7 +172,7 @@ public abstract class SetTest {
     @Test
     public void testContainsOneEntry() {
         Set<String> s1 = this.createFromArgsTest("6");
-        Set<String> s1Expected = this.createFromArgsTest("6");
+        Set<String> s1Expected = this.createFromArgsRef("6");
         boolean temp = s1.contains("6");
         assertEquals(s1Expected, s1);
         assertEquals(true, temp);
@@ -182,7 +182,7 @@ public abstract class SetTest {
     @Test
     public void testContainsNoEntries() {
         Set<String> s1 = this.createFromArgsTest();
-        Set<String> s1Expected = this.createFromArgsTest();
+        Set<String> s1Expected = this.createFromArgsRef();
         boolean temp = s1.contains("6");
         assertEquals(s1Expected, s1);
         assertEquals(false, temp);
@@ -193,7 +193,7 @@ public abstract class SetTest {
     @Test
     public void testSizeEmpty() {
         Set<String> s1 = this.createFromArgsTest();
-        Set<String> s1Expected = this.createFromArgsTest();
+        Set<String> s1Expected = this.createFromArgsRef();
         int size = s1.size();
         assertEquals(s1Expected, s1);
         assertEquals(0, size);
@@ -203,7 +203,7 @@ public abstract class SetTest {
     @Test
     public void testSizeOneEntry() {
         Set<String> s1 = this.createFromArgsTest("5");
-        Set<String> s1Expected = this.createFromArgsTest("5");
+        Set<String> s1Expected = this.createFromArgsRef("5");
         int size = s1.size();
         assertEquals(s1Expected, s1);
         assertEquals(1, size);
@@ -213,7 +213,7 @@ public abstract class SetTest {
     @Test
     public void testSizeTwoEntry() {
         Set<String> s1 = this.createFromArgsTest("5", "6");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6");
         int size = s1.size();
         assertEquals(s1Expected, s1);
         assertEquals(2, size);
@@ -223,7 +223,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveAnyTwoEntries() {
         Set<String> s1 = this.createFromArgsTest("5", "6");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6");
         String temp = s1.removeAny();
         assertEquals(true, s1Expected.contains(temp));
         s1Expected.remove(temp);
@@ -234,7 +234,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveAnyOneEntry() {
         Set<String> s1 = this.createFromArgsTest("5");
-        Set<String> s1Expected = this.createFromArgsTest("5");
+        Set<String> s1Expected = this.createFromArgsRef("5");
         String temp = s1.removeAny();
         assertEquals(true, s1Expected.contains(temp));
         s1Expected.remove(temp);
@@ -246,7 +246,7 @@ public abstract class SetTest {
     @Test
     public void testRemoveAnyThreeEntries() {
         Set<String> s1 = this.createFromArgsTest("5", "6", "7");
-        Set<String> s1Expected = this.createFromArgsTest("5", "6", "7");
+        Set<String> s1Expected = this.createFromArgsRef("5", "6", "7");
         String temp = s1.removeAny();
         assertEquals(true, s1Expected.contains(temp));
         s1Expected.remove(temp);
